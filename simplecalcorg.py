e3cb9410ee1851d34144002f3fc17e54e0ca3e27
sevenelevel <saquibmomin42@gmail.com>
2026-06-08 23:00:49 +0530

    Add files via upload

---
+++ simplecalcorg.py
@@ -0,0 +1,22 @@
+# Take two numbers from the user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+
+# Perform basic math calculations
+print("**********************************************************************************************")
+add = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+div = num1 / num2
+rem = num1 % num2
+pwr = num1 ** num2
+
+# Print the results
+print("Addition:", add)
+print("Subtraction:", sub)
+print("Multiplication:", mul)
+print("Division:", div)
+print("Modulus:", rem)
+print("Exponent:", pwr)
+print("*********************************************************************************************")
